13078d717fddaf1d48911d1e30e06789d15c15e8
A Samuel Pottinger <sam.pottinger@clarafoods.com>
2022-08-27 22:24:37 -0700

    Comments added to rand util.

--- demo/js/rand_util.js
+++ demo/js/rand_util.js
@@ -1,8 +1,32 @@
+/**
+ * Utilities for generating random values within various distributions.
+ *
+ * @license MIT
+ */
+
+
+/**
+ * Build a function which returns a value sampled from a normal distribution.
+ *
+ * @param mean The mean value of the normal distribution to be sampled.
+ * @param std The standard deviation of the normal distribution to be sampled.
+ * @returns Function which, when called, returns a random value.
+ */
 function buildNorm(mean, std) {
     return d3.randomNormal(mean, std);
 }
 
 
+/**
+ * Build a function which returns a value sampled from a log normal distribution.
+ *
+ * @param mean The mean value of the distribution to be sampled.
+ * @param std The standard deviation of the distribution to be sampled.
+ * @returns Function which, when called, returns a random value. Note that the log normal
+ *      distribution sampled has an inner mean = 0 and inner std = 0.5. The resulting value has
+ *      one subtracted and the result is multiplied by the parameter std and the parameter mean
+ *      added.
+ */
 function buildLogNorm(mean, std) {
     const inner = d3.randomLogNormal(0, 0.5)
     return () => {
@@ -12,6 +36,18 @@ function buildLogNorm(mean, std) {
 }
 
 
+/**
+ * Get a random value selected from a normal or log normal distribution.
+ *
+ * @param low The low end of the range.
+ * @param high The high end of the range.
+ * @param mustBePositive If true, negative values will be returned as zero. If false, negative
+ *      values will be returned.
+ * @param rangeStd How many standard deviations (+/- around the mean) the range made by high and
+ *      low represent in the distribution.
+ * @param useLogNorm True if log norm should be used (see buildLogNorm) and false if normal
+ *      distribution should be used.
+ */
 function getNormVal(low, high, mustBePositive, rangeStd, useLogNorm) {
     if (useLogNorm === undefined) {
         useLogNorm = false;
