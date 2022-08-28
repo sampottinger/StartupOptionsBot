b57f682b27b19f7fbc6118a80cbd2de1ee3508a3
A Samuel Pottinger <sam.pottinger@clarafoods.com>
2022-08-27 22:26:36 -0700

    Comments for shared.

--- demo/js/shared.js
+++ demo/js/shared.js
@@ -1,6 +1,18 @@
+/**
+ * Shraed objects for StartupOptionsBot.
+ *
+ * @license MIT
+ */
+
 const toolkit = StartUpOptionsBotLang.getToolkit();
 
+
+/**
+ * Format a number as en-US locale string.
+ *
+ * @param target The number to format.
+ * @returns Formatted string.
+ */
 function formatNumber(target) {
-    const self = this;
     return target.toLocaleString("en-US");
 }
